7e716a860df43239e55f157631132c8f4b10239f
hdariodev <hdariodev@gmail.com>
2023-03-29 17:55:22 -0400

    implement table

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import { Navbar } from "./components";
 import { Home } from "./pages";
+import { LayoutContainer } from "./styled-components/layout.styled.component";
 
 function App() {
   return (
-    <>
+    <LayoutContainer>
       <Navbar />
       <Home />
-    </>
+    </LayoutContainer>
   );
 }
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,74 @@
-import React from "react";
+import { People } from "@/data";
+import { Checkbox } from "@mui/material";
+import {
+  DataGrid,
+  GridColDef,
+  GridRenderCellParams,
+  GridValueGetterParams,
+} from "@mui/x-data-grid";
+
+const colums = [
+  {
+    field: "actions",
+    type: "actions",
+    sortable: false,
+    headerName: "",
+    width: 50,
+    renderCell: (params: GridRenderCellParams) => (
+      <>
+        {
+          <Checkbox />
+          // <Checkbox
+          //   size="small"
+          //   checked={findPerson(params.row)}
+          //   onChange={() => handleChange(params.row)}
+          // />
+        }
+      </>
+    ),
+  },
+  {
+    field: "name",
+    headerName: "Name",
+    flex: 1,
+    minWidth: 150,
+    renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+  },
+  {
+    field: "category",
+    headerName: "Categories",
+    flex: 1,
+    renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+  },
+  {
+    field: "company",
+    headerName: "Company",
+    flex: 1,
+    renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+  },
+  {
+    field: "levelOfHappiness",
+    headerName: "Level of happiness",
+    flex: 1,
+    renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+  },
+];
 
 const Home = () => {
-  return <div>Home</div>;
+  const rowsPerPageOptions = [5];
+
+  return (
+    <div style={{ height: 350, width: "100%" }}>
+      <DataGrid
+        rows={People}
+        columns={colums}
+        disableColumnSelector
+        pagination={true}
+        autoPageSize
+        rowsPerPageOptions={rowsPerPageOptions}
+      />
+    </div>
+  );
 };
 
 export default Home;
