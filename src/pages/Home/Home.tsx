d951243460ed04f2d38467fe57a5b0205acc91cd
hdariodev <hdariodev@gmail.com>
2023-03-29 21:16:59 -0400

    favorites

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import { People } from "@/data";
+import { Person } from "@/models";
 import { Checkbox } from "@mui/material";
 import {
   DataGrid,
@@ -6,56 +7,68 @@ import {
   GridRenderCellParams,
   GridValueGetterParams,
 } from "@mui/x-data-grid";
-
-const colums = [
-  {
-    field: "actions",
-    type: "actions",
-    sortable: false,
-    headerName: "",
-    width: 50,
-    renderCell: (params: GridRenderCellParams) => (
-      <>
-        {
-          <Checkbox />
-          // <Checkbox
-          //   size="small"
-          //   checked={findPerson(params.row)}
-          //   onChange={() => handleChange(params.row)}
-          // />
-        }
-      </>
-    ),
-  },
-  {
-    field: "name",
-    headerName: "Name",
-    flex: 1,
-    minWidth: 150,
-    renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
-  },
-  {
-    field: "category",
-    headerName: "Categories",
-    flex: 1,
-    renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
-  },
-  {
-    field: "company",
-    headerName: "Company",
-    flex: 1,
-    renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
-  },
-  {
-    field: "levelOfHappiness",
-    headerName: "Level of happiness",
-    flex: 1,
-    renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
-  },
-];
+import { useState } from "react";
 
 const Home = () => {
+  const [selectedPeople, setSelectedPeople] = useState<Person[]>([]);
   const rowsPerPageOptions = [5];
+  const findPerson = (person: Person) =>
+    !!selectedPeople.find((p) => p.id === person.id);
+  const filterPerson = (person: Person) =>
+    selectedPeople.filter((p) => p.id !== person.id);
+
+  const handleChange = (person: Person) => {
+    const filteredPeople = findPerson(person)
+      ? filterPerson(person)
+      : [...selectedPeople, person];
+    setSelectedPeople(filteredPeople);
+  };
+
+  const colums = [
+    {
+      field: "actions",
+      type: "actions",
+      sortable: false,
+      headerName: "",
+      width: 50,
+      renderCell: (params: GridRenderCellParams) => (
+        <>
+          {
+            <Checkbox
+              size="small"
+              checked={findPerson(params.row)}
+              onChange={() => handleChange(params.row)}
+            />
+          }
+        </>
+      ),
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: "category",
+      headerName: "Categories",
+      flex: 1,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: "company",
+      headerName: "Company",
+      flex: 1,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: "levelOfHappiness",
+      headerName: "Level of happiness",
+      flex: 1,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+  ];
 
   return (
     <div style={{ height: 350, width: "100%" }}>
